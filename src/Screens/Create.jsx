22d153bc19102c7140aadc30351a8a1419936cca
Tausif <tausif00100@gmail.com>
2024-05-27 04:18:46 +0530

    created Createscreen

--- src/Screens/Create.jsx
+++ src/Screens/Create.jsx
@@ -1,15 +1,22 @@
 import { StyleSheet, Text, View  } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { COLORS } from './Utils/Colors'
 
 const Create = () => {
   return (
-    <SafeAreaView>
-      <Text>Create</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={{color:'white'}}>Create</Text>
     </SafeAreaView>
   )
 }
 
 export default Create
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: COLORS.PRIMARY,
+    padding: 20
+  }
+})
